ffaa45b9358d5e8c229534ceec20747fb0afb485
Amrit64 <amrit6446@gmail.com>
2018-11-28 15:08:58 +0530

    Update config.php

--- garbage/config.php
+++ garbage/config.php
@@ -13,30 +13,7 @@
     function __destruct() {  
 
     }  
-// public function Add(){   
-//  // if(isset($_GET['id'])) {
-//        $sql = "SELECT * FROM employee"; 
-//         $result = $this->db->query($sql);
-//         // return $sql;  
 
-
-//                If(mysqli_num_rows($result)>0)
-//                {
-//                  while($row=mysqli_fetch_array($result))
-//                  {  
-//                      // echo $row['uid'];
-//                      // echo $row['username']; 
-//                      // echo "<td>" . $row['uid'] . "</td>";
-//                      //  echo "<td>" . $row['username'] . "</td>";
-
-// }
-// 	} 
-//  }
- //    public function select()
- // {
- //  $res=mysql_query("SELECT * FROM employee");
- //  return $res;
- // }
     public function fetchdata()
 {
 $result=mysqli_query($this->db,"select * from employee");
@@ -45,12 +22,3 @@ return $result;
 }
 ?>
 
-
-<!-- $sql = " SELECT candidate.cand_number,candidate.cand_fname,candidate.cand_desc FROM candidate ".$join.' where '.$condition;
-$result = mysql_query($sql) or die(mysql_error()); 
-while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
-      // echo "EMP ID :{$row['emp_id']}  <br> ".
-      //    "EMP NAME : {$row['emp_name']} <br> ".
-      //    "EMP SALARY : {$row['emp_salary']} <br> ".
-      //    "--------------------------------<br>";
-   } -->
